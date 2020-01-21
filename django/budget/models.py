566afe483beda9dd87ab92d4781f154d279cd03b
retarf <retarf@gmail.com>
2020-01-21 19:04:49 +0100

    budget app created. Entry model added.

---
+++ django/budget/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Entry(models.Model):
+    entry_creation_date = models.DateField(auto_now_add=True)
+    entry_modification_date = models.DateField(auto_now=True)
+    month = models.DateField()
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    value = models.FloatField()
