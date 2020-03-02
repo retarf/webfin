8076fa3527467cc1f17d3c12b91cc2775dc51b00
retarf <retarf@gmail.com>
2020-03-02 12:16:10 +0100

    backend: Month GET request added.

--- backend/budget/api.py
+++ backend/budget/api.py
@@ -6,3 +6,9 @@ from .serializers import EntrySerializer
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+
+    def get_queryset(self):
+        month = self.request.query_params.get('month', None)
+        month += '-01'
+        return Entry.objects.filter(month=month)
+
