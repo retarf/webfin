46711425bdf9f87ccccddabe8d95a89417e9c707
retarf <retarf@gmail.com>
2020-04-11 19:54:36 +0200

    [backend] Month validation added.

--- backend/budget/api.py
+++ backend/budget/api.py
@@ -1,26 +1,46 @@
 import datetime
 
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from .models import Entry
 from .serializers import EntrySerializer
 
-FORMAT = '%Y-%m-%d'
+FORMAT = '%Y-%m-01'
+
+def is_month_valid(month):
+
+    result = True
+    if int(month[0]) > 1:
+        result = False
+    if int(month[0]) == 1 and int(month[1]) > 2:
+        result = False
+
+    return result
 
 class EntryViewSet(viewsets.ModelViewSet):
 
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
 
-    def get_queryset(self):
+    def list(self, request):
+
         month = self.request.query_params.get('month', None)
+
         if month is None:
             now = datetime.datetime.now().strftime(FORMAT)
         else:
+            month_number = month.split('-')[1]
+            if not is_month_valid(month_number):
+                return Response('Bad request format. Request valid format is RRRR-MM', status=status.HTTP_406_NOT_ACCEPTABLE)
             month += '-01'
 
-        return Entry.objects.filter(month=month)
+        queryset = Entry.objects.filter(month=month)
+        serializer = EntrySerializer(queryset, many=True)
+
+        return Response(serializer.data, stauts=status.HTTP_200_OK)
 
     def create(self, request, format=None):
         request.data['month'] += '-01'
@@ -28,5 +48,5 @@ class EntryViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
 
-        return Response(request.data)
+        return Response(request.data, status=status.HTTP_201_CREATED)
 
