03f61e18b7f1bc23f152d9e3df04239f65aafe37
retarf <retarf@gmail.com>
2020-04-11 13:43:34 +0200

    [frontent] Regex added to MonthForm component

--- frontend/src/components/MonthForm.js
+++ frontend/src/components/MonthForm.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
+const MonthRegEx = /^([0-9]{4}-[0-9]{2})$/
+
 class MonthForm extends Component {
     constructor () {
         super();
         this.state = {
             month: '',
+            hasError: null,
         };
     };
+
+    // componentDidUpdate (prevState) {
+    //     console.log('DidErr', this.state.hasError);
+    // };
+
+    // componentWillUpdate(prevState) {
+    //     console.log('willErr', this.state.hasError);
+    // };
 
     handleChange = event => {
         this.setState({ month: event.target.value }); 
@@ -17,8 +28,19 @@ class MonthForm extends Component {
         event.preventDefault();
 
         const month = this.state.month;
-        this.props.setMonth(month);
-        this.resetForm();
+        if (MonthRegEx.test(month)) {
+            this.props.setMonth(month);
+            this.resetForm();
+            this.setState({ hasError: false });
+        }
+        else if (month === null) {
+            this.setState({ hasError: false });
+        }
+        else
+        {
+            this.setState({ hasError: true });
+        };
+
     };
 
     resetForm = () => {
